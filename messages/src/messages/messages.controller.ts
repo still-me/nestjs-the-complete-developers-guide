66dabad7aa236bdc8f956b26fe9c5963f3037373
Vladyslav Fomenko <71770160+still-me@users.noreply.github.com>
2025-02-24 20:44:43 +0200

    1 messages (#1)
    * Base
    
    * Dependency Injection

---
+++ messages/src/messages/messages.controller.ts
@@ -0,0 +1,35 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
+
+import CreateMessageDTO from './dtos/create-message.dto';
+import { MessagesService } from './messages.servive';
+
+@Controller('messages')
+export class MessagesController {
+  constructor(public messagesService: MessagesService) {}
+
+  @Get()
+  listMessages() {
+    return this.messagesService.findAll();
+  }
+
+  @Post()
+  createMessage(@Body() body: CreateMessageDTO) {
+    return this.messagesService.create(body.content);
+  }
+
+  @Get('/:id')
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+
+    if (!message) throw new NotFoundException('Message not');
+
+    return message;
+  }
+}
---
+++ messages/src/messages/messages.repository.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { readFile, writeFile } from 'fs/promises';
+
+@Injectable()
+export class MessagesRepository {
+  async findOne(id: string) {
+    const content = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(content);
+    return messages[id];
+  }
+
+  async findAll() {
+    const content = await readFile('messages.json', 'utf8');
+    return JSON.parse(content);
+  }
+
+  async create(content: string) {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
+    const id = Math.floor(Math.random() * 999);
+    messages[id] = { id, content };
+    await writeFile('messages.json', JSON.stringify(messages));
+  }
+}
