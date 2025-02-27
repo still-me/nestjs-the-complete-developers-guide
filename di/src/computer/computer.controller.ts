6f33755a18d2ed4d01a8a09a8329a17bedc06a47
Fomenko Vladyslav <fomenkovlad93@gmail.com>
2025-02-27 23:37:51 +0200

    connect  ComputerModule => CpuModule + DiskModule => PowerModule

--- di/src/computer/computer.controller.ts
+++ di/src/computer/computer.controller.ts
@@ -1,4 +1,17 @@
 import { Controller } from '@nestjs/common';
+import { CpuService } from 'src/cpu/cpu.service';
+import { DiskService } from 'src/disk/disk.service';
+import { Get } from '@nestjs/common';
 
 @Controller('computer')
-export class ComputerController {}
+export class ComputerController {
+  constructor(
+    private cpuService: CpuService,
+    private diskService: DiskService,
+  ) {}
+
+  @Get()
+  run() {
+    return [this.cpuService.compute(1, 2), this.diskService.getData()];
+  }
+}
